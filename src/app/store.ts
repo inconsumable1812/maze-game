b3e2c30c1412fe412f190381c85c46072ac13ad2
Inconsumable <inconsumable1812@gmail.com>
2022-07-18 20:26:58 +0800

    del unused

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { reducer as mazeReducer } from '../features/maze';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     maze: mazeReducer
   }
 });
